8a4695e3f0f17dd55c371887b3432c48bd225de4
bricknest2025 <office@bricknestventures.com>
2025-08-06 18:50:20 -0400

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+```tsx
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="text-center text-sm text-gray-500 mt-10">
+      &copy; {new Date().getFullYear()} Bricknest Ventures LLC. All rights reserved.
+    </footer>
+  );
+}
+```
